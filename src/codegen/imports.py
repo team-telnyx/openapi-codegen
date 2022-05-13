c793c813e0c1894f915648f20c4092b5926dee02
Charles Hall <charles@computer.surgery>
2022-05-12 21:03:32 -0700

    create custom `BaseModel` for consistent `Config`s
    See [here][0] for details on why you'd want to do this.
    
    [0]: http://charles.page.computer.surgery/blog/python-pydantic-pitfalls.html

--- src/codegen/imports.py
+++ src/codegen/imports.py
@@ -2,3 +2,7 @@ from typing import Optional, List, Set, Dict, Any
 
 import aiohttp
 from pydantic import BaseModel, Field
+
+class _BaseModel(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
