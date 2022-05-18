aa5adfc2c666fd5201077fa2fc5335bc9372902f
Charles Hall <charles@computer.surgery>
2022-05-18 14:19:03 -0700

    rewrite function codegen
    `write{,ln}!()` macro was very verbose with all of its `.expect()`s and
    having everything in one function was becoming unwieldy. Now, each part
    of the codegen is separated into its own function, making it much easier
    to make changes to one individual part of the code at a time without
    messing up everything else. Also, I fixed an issue (well, deliberate
    oversight) where all API calls were using GET requests.

--- src/codegen/functions.rs
+++ src/codegen/functions.rs
@@ -1,12 +1,12 @@
 //! Generate code for HTTP methods
 
-use std::{collections::HashMap, fmt::Write};
+use std::collections::HashMap;
 
 use heck::ToSnakeCase;
 use okapi::openapi3::OpenApi;
 
 use super::type_to_string;
-use crate::parse::{Function, SecurityScheme};
+use crate::parse::{Argument, Function, SecurityScheme, Type};
 
 /// Generates a function for each method available on each HTTP path
 // TODO: remove this when more HTTP auth methods are implemented
@@ -20,95 +20,149 @@ pub fn functions(
     let mut code = String::new();
 
     for ((method, path), function) in fs {
-        let fn_ident = format!("{method}_{path}").to_snake_case();
-
-        let return_code = {
-            let return_types = function
-                .responses
-                .responses
-                .iter()
-                .map(|(_code, ty)| ty)
-                .cloned()
-                .collect::<Vec<_>>();
-
-            match return_types.as_slice() {
-                [] => String::from("None"),
-                [x] => type_to_string(x, false),
-                _ => {
-                    let mut return_code = return_types
-                        .into_iter()
-                        .map(|x| type_to_string(&x, false))
-                        .fold(String::from("Union["), |mut acc, x| {
-                            acc.push_str(&x);
-                            acc.push_str(", ");
-
-                            acc
-                        });
-
-                    return_code.push(']');
-
-                    return_code
-                }
-            }
-        };
-
-        let args =
-            function.arguments.iter().fold(String::default(), |mut acc, x| {
-                write!(
-                    &mut acc,
-                    "{}: {}, ",
-                    x.name,
-                    type_to_string(&x.r#type, false)
-                )
-                .expect("write failed");
-
-                acc
-            });
-
-        writeln!(
-            &mut code,
-            "{}async def {}(self, {}) -> {}:",
-            super::INDENT,
-            fn_ident,
-            args,
-            return_code,
-        )
-        .expect("write failed");
-
-        if let Some(docs) = function.docs {
-            writeln!(
-                &mut code,
-                "{i}{i}\"\"\"{}\"\"\"",
-                docs,
-                i = super::INDENT,
-            )
-            .expect("write failed");
-        } else {
-            writeln!(&mut code, "{i}{i}pass", i = super::INDENT)
-                .expect("write failed");
-        }
-
-        // A list of methods this request can be authenticated by
-        let mut schemes = function
-            .security_schemes
-            .iter()
-            .filter_map(|name| security_schemes.get(name))
-            .copied();
-
-        write!(
-            &mut code,
-            "{i}{i}resp = await self._session.get(f\"{{self._base_url}}{}\", ",
-            path,
-            i = super::INDENT,
-        )
-        .expect("write failed");
-
-        if schemes.any(|x| x == SecurityScheme::BasicAuth) {
-            write!(&mut code, "auth=self._auth").expect("write failed");
-        }
-
-        writeln!(&mut code, ")\n").expect("write failed");
+        code.push_str(&signature(1, &method, &path, &function));
+        code.push_str(&documentation(2, function.docs.as_ref()));
+        code.push_str(&body(2, method, path, &function, security_schemes));
+        code.push('\n');
     }
 
     code
+}
+
+/// Generate an amount of indentations
+fn indents(indent_level: usize) -> String {
+    (0..indent_level).into_iter().fold(String::new(), |mut acc, _| {
+        acc.push_str(super::INDENT);
+        acc
+    })
+}
+
+/// Generate the body of a function
+// TODO: remove this when more HTTP auth methods are implemented
+#[allow(clippy::zero_sized_map_values)]
+fn body<S1, S2>(
+    indent_level: usize,
+    method: S1,
+    path: S2,
+    function: &Function,
+    security_schemes: &HashMap<String, SecurityScheme>,
+) -> String
+where
+    S1: AsRef<str>,
+    S2: AsRef<str>,
+{
+    let method = method.as_ref();
+    let path = path.as_ref();
+
+    // A list of methods this request can be authenticated by
+    let mut schemes = function
+        .security_schemes
+        .iter()
+        .filter_map(|name| security_schemes.get(name))
+        .copied();
+
+    let auth_args = if schemes.any(|x| x == SecurityScheme::BasicAuth) {
+        "auth=self._auth"
+    } else {
+        ""
+    };
+
+    format!(
+        "{i}resp = await \
+         self._session.{method}(f\"{{self._base_url}}{path}\", {auth_args})",
+        i = indents(indent_level),
+    )
+}
+
+/// Generate the documentation for a function
+fn documentation<S: AsRef<str>>(
+    indent_level: usize,
+    docs: Option<S>,
+) -> String {
+    match docs {
+        Some(x) => {
+            format!(
+                "{i}\"\"\"\n{}\n{i}\"\"\"\n",
+                x.as_ref(),
+                i = indents(indent_level)
+            )
+        }
+        None => format!("{i}\"\"\"\n{i}\"\"\"\n", i = indents(indent_level)),
+    }
+}
+
+/// Generate an entire function signature
+///
+/// For example, `async def get_foo_foo_id(foo_id: str) -> Foo:`. This includes
+/// the requested indentation and a single trailing newline.
+fn signature<S1, S2>(
+    indent_level: usize,
+    method: S1,
+    path: S2,
+    function: &Function,
+) -> String
+where
+    S1: AsRef<str>,
+    S2: AsRef<str>,
+{
+    format!(
+        "{i}async def {}(self, {}) -> {}:\n",
+        name(method, path),
+        arguments(function.arguments.iter()),
+        return_type(&function.responses.responses),
+        i = indents(indent_level),
+    )
+}
+
+/// Generate the name of a function
+fn name<S1, S2>(method: S1, path: S2) -> String
+where
+    S1: AsRef<str>,
+    S2: AsRef<str>,
+{
+    format!("{}_{}", method.as_ref(), path.as_ref()).to_snake_case()
+}
+
+/// Generate the arguments that a function takes
+///
+/// These go between the `(` and `)`. Return value will not contain any
+/// newlines.
+fn arguments<'a, I: Iterator<Item = &'a Argument>>(arguments: I) -> String {
+    arguments.fold(String::default(), |mut acc, x| {
+        acc.push_str(&format!(
+            "{}: {}, ",
+            &x.name,
+            type_to_string(&x.r#type, false)
+        ));
+        acc
+    })
+}
+
+/// Generate the type that a function returns
+///
+/// This is the part that goes between the `->` and the `:`. Return value will
+/// not contain any newlines.
+fn return_type(responses: &HashMap<String, Type>) -> String {
+    let return_types =
+        responses.iter().map(|(_code, ty)| ty).cloned().collect::<Vec<_>>();
+
+    match return_types.as_slice() {
+        [] => String::from("None"),
+        [x] => type_to_string(x, false),
+        _ => {
+            let mut return_code = return_types
+                .into_iter()
+                .map(|x| type_to_string(&x, false))
+                .fold(String::from("Union["), |mut acc, x| {
+                    acc.push_str(&x);
+                    acc.push_str(", ");
+
+                    acc
+                });
+
+            return_code.push(']');
+
+            return_code
+        }
+    }
 }
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -10,7 +10,7 @@ mod r#type;
 
 pub use error::{Parse as Error, ParseKind as ErrorKind};
 pub use field::Field;
-pub use function::Function;
+pub use function::{Argument, Function};
 pub use r#struct::Struct;
 pub use r#type::Type;
 pub use responses::Responses;
